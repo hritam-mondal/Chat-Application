1a7cc06edc210360f7c64b6b5981bf4bb40dc955
Hritam Mondal <hritamcs@outlook.com>
2020-08-02 17:40:36 +0530

    Add files via upload

---
+++ client/build/precache-manifest.143d11b3d98a276dbdcd5319508ba7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba02c33a73d6f30906576375830b6f6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da2c0558e8e0aa52c4cb",
+    "url": "/static/css/main.840e23fd.chunk.css"
+  },
+  {
+    "revision": "bb58ac565d57fb87aa40",
+    "url": "/static/js/2.925f777a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.925f777a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2c0558e8e0aa52c4cb",
+    "url": "/static/js/main.a19c696e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
